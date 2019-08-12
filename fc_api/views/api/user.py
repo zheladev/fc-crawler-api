e3c33d95b850e5ef172cf7d573213de052a57185
Aitor <zheladev@gmail.com>
2019-08-13 00:54:43 +0200

    Add user filtering

--- fc_api/views/api/user.py
+++ fc_api/views/api/user.py
@@ -1,6 +1,8 @@
 from pyramid.view import view_defaults, view_config
 
 from fc_api.core.container import Repositories
+from fc_api.lib.validation import validate_data, DataType
+from fc_api.lib.validation_schemas.user import User as User_validator
 from fc_api.repositories.user import UserRepository
 from fc_api.views.serializer import serialize_user
 
@@ -18,7 +20,10 @@ class UserAPI:
         return serialize_user(user)
 
     @view_config(route_name='users', request_method='GET')
+    @validate_data(User_validator.receiver_param(),
+                   data_type=DataType.URL_PARAM.value)
     def list(self):
-        users = self._user_repository.list(limit=25)
+        params = self._request.validated_url_params
+        users = self._user_repository.list(**params)
 
         return [serialize_user(user) for user in users]
